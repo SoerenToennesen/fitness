aacd254e03c06af3030dc50b6feed745f2ab0afa
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-03-12 15:36:12 +0100

    cleaned Exercise.tsx a bit more

--- ui/src/Components/Exercise/Exercise.tsx
+++ ui/src/Components/Exercise/Exercise.tsx
@@ -5,57 +5,34 @@ import {api_urls} from '../../Api_urls'
 interface MyProps {
 }
 interface MyStates {
-    exercises: any,
-    modalTitle: string,
-    ExerciseId: number,
-    ExerciseName: string,
-    ExerciseIdFilter: string,
-    ExerciseNameFilter: string,
-    exercisesWithoutFilter: [],
     options: any,
     series: any,
 }
 
 export class Exercise extends Component<MyProps, MyStates> {
-
     constructor(props: any) {
         super(props);
         this.state={
-            exercises: [],
-            modalTitle: "",
-            ExerciseId: 0,
-            ExerciseName: "",
-
-            ExerciseIdFilter: "",
-            ExerciseNameFilter: "",
-            exercisesWithoutFilter: [],
-
             // y color strength values are taken relative to each other
             series: [{
                 name: 'Sun',
                 data: this.generateData(52)
-            },
-            {
+            },{
                 name: 'Mon',
                 data: this.generateData(52)
-            },
-            {
+            },{
                 name: 'Tue',
                 data: this.generateData(52)
-            },
-            {
+            },{
                 name: 'Wed',
                 data: this.generateData(52)
-            },
-            {
+            },{
                 name: 'Thu',
                 data: this.generateData(52)
-            },
-            {
+            },{
                 name: 'Fri',
                 data: this.generateData(52)
-            },
-            {
+            },{
                 name: 'Sat',
                 data: this.generateData(52)
             }],
