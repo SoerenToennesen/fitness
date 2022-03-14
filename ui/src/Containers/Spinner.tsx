08582036eb004eb1eb0130b9bd8ec13ce16469f8
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-03-14 23:36:39 +0100

    centered spinner

--- ui/src/Containers/Spinner.tsx
+++ ui/src/Containers/Spinner.tsx
@@ -3,7 +3,19 @@ import './css/Spinner.css';
 
 export function Spinner() {
     return (
-        <div className="running">
+        <div
+            className="running"
+            style={{
+                width: "100px",
+                height: "100px",
+                position: "absolute",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                margin: "auto",
+            }}
+        >
             <div className="outer">
                 <div className="runnerbody">
                     <div className="arm behind"></div>
