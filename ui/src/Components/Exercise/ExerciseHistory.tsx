5324efc6921787a22b076e6a4bc40963262b9a5b
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-03-16 23:23:27 +0100

    minor code smell fixes

--- ui/src/Components/Exercise/ExerciseHistory.tsx
+++ ui/src/Components/Exercise/ExerciseHistory.tsx
@@ -311,50 +311,50 @@ export class ExerciseHistory extends Component<MyProps, MyStates> {
     tableData() {
         return (
             <tbody>
-            {this.state.exercises.map((exe : any) =>
-                <tr key={exe.id}>
-                    <td>{exe.exerciseType}</td>
-                    <td>{exe.exerciseLength + ' minutes'}</td>
-                    <td>{exe.exerciseTime}</td>
-                    <td>{exe.caloriesBurned}</td>
-                    <td>
-                        <button
-                            type="button"
-                            className="btn btn-light mr-1"
-                            data-bs-toggle="modal"
-                            data-bs-target="#exampleModal"
-                            onClick={() => {
-                                this.setState({
-                                    modalData: {...this.state.modalData, buttonTitle: 'Update'}
-                                });
-                            }}
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                            </svg>
-                        </button>
-                        <button
-                            type="button"
-                            className="btn btn-light mr-1"
-                            onClick={() => {
-                                this.setState({
-                                    confirmModal: {
-                                        isOpen: true,
-                                        title: 'Do you want to delete exercise ' + exe.exerciseType + ' for ' + exe.exerciseLength + ' minutes at ' + exe.exerciseTime + '?',
-                                        subTitle: 'This will permanently delete this record.',
-                                        onConfirm: () => {this.deleteClick(exe.id)}
-                                    }
-                                });
-                            }}
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                            </svg>
-                        </button>
-                    </td>
-                </tr>
-            )}
+                {this.state.exercises.map((exe : any) =>
+                    <tr key={exe.id}>
+                        <td>{exe.exerciseType}</td>
+                        <td>{exe.exerciseLength + ' minutes'}</td>
+                        <td>{exe.exerciseTime}</td>
+                        <td>{exe.caloriesBurned}</td>
+                        <td>
+                            <button
+                                type="button"
+                                className="btn btn-light mr-1"
+                                data-bs-toggle="modal"
+                                data-bs-target="#exampleModal"
+                                onClick={() => {
+                                    this.setState({
+                                        modalData: {...this.state.modalData, buttonTitle: 'Update'}
+                                    });
+                                }}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                </svg>
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-light mr-1"
+                                onClick={() => {
+                                    this.setState({
+                                        confirmModal: {
+                                            isOpen: true,
+                                            title: 'Do you want to delete exercise ' + exe.exerciseType + ' for ' + exe.exerciseLength + ' minutes at ' + exe.exerciseTime + '?',
+                                            subTitle: 'This will permanently delete this record.',
+                                            onConfirm: () => {this.deleteClick(exe.id)}
+                                        }
+                                    });
+                                }}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                </svg>
+                            </button>
+                        </td>
+                    </tr>
+                )}
             </tbody>
         )
     }
@@ -479,6 +479,7 @@ export class ExerciseHistory extends Component<MyProps, MyStates> {
         return (
             <div>
                 <div className="page-header">Exercise History</div>
+                {/*TODO: All below should be refactored into a table component*/}
                 {this.exerciseTable()}
                 <AddUpdateModal
                     modalData={this.state.modalData}
--- ui/src/Components/Nutrition/NutritionHistory.tsx
+++ ui/src/Components/Nutrition/NutritionHistory.tsx
@@ -282,51 +282,53 @@ export class NutritionHistory extends Component<MyProps, MyStates> {
     }
 
     tableData() {
-        <tbody>
-        {this.state.nutritions.map((nut : any) =>
-            <tr key={nut.id}>
-                <td>{nut.nutritionType}</td>
-                <td>{nut.injestionTime}</td>
-                <td>{nut.calories}</td>
-                <td>
-                    <button
-                        type="button"
-                        className="btn btn-light mr-1"
-                        data-bs-toggle="modal"
-                        data-bs-target="#exampleModal"
-                        onClick={() => {
-                            this.setState({
-                                modalData: {...this.state.modalData, buttonTitle: 'Update'}
-                            });
-                        }}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                            <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                        </svg>
-                    </button>
-                    <button
-                        type="button"
-                        className="btn btn-light mr-1"
-                        onClick={() => {
-                            this.setState({
-                                confirmModal: {
-                                    isOpen: true,
-                                    title: 'Do you want to delete nutrition ' + nut.nutritionType + ' at ' + nut.injestionTime + '?',
-                                    subTitle: 'This will permanently delete this record.',
-                                    onConfirm: () => {this.deleteClick(nut.id)}
-                                }
-                            });
-                        }}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                        </svg>
-                    </button>
-                </td>
-            </tr>
-        )}
-        </tbody>
+        return (
+            <tbody>
+                {this.state.nutritions.map((nut : any) =>
+                    <tr key={nut.id}>
+                        <td>{nut.nutritionType}</td>
+                        <td>{nut.injestionTime}</td>
+                        <td>{nut.calories}</td>
+                        <td>
+                            <button
+                                type="button"
+                                className="btn btn-light mr-1"
+                                data-bs-toggle="modal"
+                                data-bs-target="#exampleModal"
+                                onClick={() => {
+                                    this.setState({
+                                        modalData: {...this.state.modalData, buttonTitle: 'Update'}
+                                    });
+                                }}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                </svg>
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-light mr-1"
+                                onClick={() => {
+                                    this.setState({
+                                        confirmModal: {
+                                            isOpen: true,
+                                            title: 'Do you want to delete nutrition ' + nut.nutritionType + ' at ' + nut.injestionTime + '?',
+                                            subTitle: 'This will permanently delete this record.',
+                                            onConfirm: () => {this.deleteClick(nut.id)}
+                                        }
+                                    });
+                                }}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                </svg>
+                            </button>
+                        </td>
+                    </tr>
+                )}
+            </tbody>
+        )
     }
 
     nutritionTable() {
